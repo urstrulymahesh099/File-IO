4b0fa6da4ae2593757b7bf680de0c596a84c6c21
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-30 18:36:55 +0530

    [Mahesh]Add:FileExistsAndReadAllLines

---
+++ File_IO/FileIO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    public class FileIO
+    {
+        public static void FileExist()
+        {
+            string path = @"E:\Bridgelabz\File-IO\File_IO\TextFile1.txt";
+            if(File.Exists(path))
+            {
+                Console.WriteLine("File Exists");
+            }
+        }
+        public static void ReadAllLines()
+        {
+            string path = @"E:\Bridgelabz\File-IO\File_IO\TextFile1.txt";
+            string[] lines;
+            lines = File.ReadAllLines(path);
+            Console.WriteLine(lines[0]);
+            Console.WriteLine(lines[1]);
+        }
+        
+    }
+}
--- File_IO/Program.cs
+++ File_IO/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File_IO problem");
+            while(true)
+            {
+                Console.WriteLine("---------------------------------------------\nPlease Select Below Given Options\n1.FileExists\n2.ReadAlllines");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        FileIO.FileExist();
+                        break;
+                    case 2:
+                        FileIO.ReadAllLines();
+                        break;
+                }
+            }
+        }
+    }
+}
